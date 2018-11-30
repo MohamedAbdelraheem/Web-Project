c761c955cbe9d3ae55e03143fe664da2f9772dab
mohamed Abdalraheem <mohamed Abdalraheem>
2018-11-30 18:39:35 +0200

    Project

---
+++ shop.sql
@@ -0,0 +1,206 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 30, 2018 at 03:54 PM
+-- Server version: 10.1.28-MariaDB
+-- PHP Version: 7.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `shop`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+
+CREATE TABLE `categories` (
+  `ID` int(11) NOT NULL,
+  `Name` varchar(255) NOT NULL,
+  `Description` text NOT NULL,
+  `parent` int(11) NOT NULL,
+  `Ordering` int(11) DEFAULT NULL,
+  `Visibility` tinyint(4) NOT NULL DEFAULT '0',
+  `Allow_Comment` tinyint(4) NOT NULL DEFAULT '0',
+  `Allow_Ads` tinyint(4) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`ID`, `Name`, `Description`, `parent`, `Ordering`, `Visibility`, `Allow_Comment`, `Allow_Ads`) VALUES
+(11, 'Clothing', 'Clothing And Fashion', 0, 4, 1, 0, 0),
+(12, 'Jens', '', 11, 50, 1, 0, 0),
+(13, 'Shirts', '', 11, 2, 1, 0, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `comments`
+--
+
+CREATE TABLE `comments` (
+  `c_id` int(11) NOT NULL,
+  `comment` text NOT NULL,
+  `status` tinyint(4) NOT NULL,
+  `comment_date` date NOT NULL,
+  `item_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `items`
+--
+
+CREATE TABLE `items` (
+  `Item_ID` int(11) NOT NULL,
+  `Name` varchar(255) NOT NULL,
+  `Description` text NOT NULL,
+  `Price` varchar(255) NOT NULL,
+  `Add_Date` date NOT NULL,
+  `Country_Made` varchar(255) NOT NULL,
+  `Image` varchar(255) NOT NULL,
+  `Status` varchar(255) NOT NULL,
+  `Rating` smallint(6) NOT NULL,
+  `Approve` tinyint(4) NOT NULL DEFAULT '0',
+  `Cat_ID` int(11) NOT NULL,
+  `Member_ID` int(11) NOT NULL,
+  `tags` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `items`
+--
+
+INSERT INTO `items` (`Item_ID`, `Name`, `Description`, `Price`, `Add_Date`, `Country_Made`, `Image`, `Status`, `Rating`, `Approve`, `Cat_ID`, `Member_ID`, `tags`) VALUES
+(35, 'بنطلون ميلتون', 'بنطلون ميلتون شتوى خامة تقيلة', '120', '2018-11-29', 'مصر', '', '1', 0, 1, 11, 35, 'ميلتون , شروال'),
+(37, 'تيشرت بولو', 'تيشرت نص كم قطن 100% ', '90', '2018-11-30', 'مصر', '', '1', 0, 1, 13, 33, ''),
+(38, 'تيشرتات خريفى D&G', 'تيشرتات خريفى قطن 100% ', '140', '2018-11-30', 'مصر', '', '1', 0, 1, 13, 33, '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `UserID` int(11) NOT NULL COMMENT 'To Identify User',
+  `Username` varchar(255) NOT NULL COMMENT 'Username To Login',
+  `Password` varchar(255) NOT NULL COMMENT 'Password To Login',
+  `Email` varchar(255) NOT NULL,
+  `FullName` varchar(255) NOT NULL,
+  `GroupID` int(11) NOT NULL DEFAULT '0' COMMENT 'Identify User Group',
+  `RegStatus` int(11) NOT NULL DEFAULT '0' COMMENT 'Seller Rank',
+  `Date` date NOT NULL,
+  `avatar` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`UserID`, `Username`, `Password`, `Email`, `FullName`, `GroupID`, `RegStatus`, `Date`, `avatar`) VALUES
+(33, 'admin', 'f7c3bc1d808e04732adf679965ccc34ca7ae3441', 'admin@admin', 'Admin', 1, 1, '0000-00-00', ''),
+(35, 'MohamedAbdalraheem', '7c4a8d09ca3762af61e59520943dc26494f8941b', 'mohamedabdo644540@gmail.com', '', 0, 1, '2018-11-29', '');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`ID`),
+  ADD UNIQUE KEY `Name` (`Name`);
+
+--
+-- Indexes for table `comments`
+--
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`c_id`),
+  ADD KEY `items_comment` (`item_id`),
+  ADD KEY `comment_user` (`user_id`);
+
+--
+-- Indexes for table `items`
+--
+ALTER TABLE `items`
+  ADD PRIMARY KEY (`Item_ID`),
+  ADD KEY `member_1` (`Member_ID`),
+  ADD KEY `cat_1` (`Cat_ID`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`UserID`),
+  ADD UNIQUE KEY `Username` (`Username`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT for table `comments`
+--
+ALTER TABLE `comments`
+  MODIFY `c_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `items`
+--
+ALTER TABLE `items`
+  MODIFY `Item_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `UserID` int(11) NOT NULL AUTO_INCREMENT COMMENT 'To Identify User', AUTO_INCREMENT=37;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `comments`
+--
+ALTER TABLE `comments`
+  ADD CONSTRAINT `comment_user` FOREIGN KEY (`user_id`) REFERENCES `users` (`UserID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `items_comment` FOREIGN KEY (`item_id`) REFERENCES `items` (`Item_ID`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `items`
+--
+ALTER TABLE `items`
+  ADD CONSTRAINT `cat_1` FOREIGN KEY (`Cat_ID`) REFERENCES `categories` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `member_1` FOREIGN KEY (`Member_ID`) REFERENCES `users` (`UserID`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
